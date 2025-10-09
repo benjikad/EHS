a27eaacb757b984a27b94b130b1c7b176920c312
benjikad <123773963+benjikad@users.noreply.github.com>
2025-10-08 20:55:33 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,5 +1,30 @@
---v1.1
+--v1.2
 return function(shared)
-  print('heheheh')
-  warn('this is working!!')
+    local modules = shared.modules
+    local theme = shared.theme
+    local mainUi = shared.mainUi
+
+    local lib = modules['UiLibrary.lua'](shared)
+
+    repeat
+        task.wait()
+    until shared.ready
+
+    local ui = lib.new(mainUi)
+
+    -- local sections = {
+    --     ui:newSection('About'),
+    --     ui:newSection('Universal'),
+    --     ui:newSection('Settings'),
+    -- }
+
+    -- ui:open(sections[1])
+
+    -- sections[1]:newLabel('<b>What is this?</b>\n This is a test.',{
+    --     RichText = true
+    -- })
+
+    -- sections[3]:newButton('Something broken?','Relaunch EHS',function()
+    --     print('UHH this doesnt work yet..')
+    -- end)
 end
